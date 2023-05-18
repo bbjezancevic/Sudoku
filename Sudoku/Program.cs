93e470f94f81f42a42d4179445c1ca0d5b1835f5
Bozo Bjezancevic <bozo.bjezancevic@gmail.com>
2023-05-18 12:42:41 +0200

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            //int[,] zagonetka = new int[,] {
+            //{ 0, 0, 0, 0, 3, 8, 0, 4 },
+            //{ 0, 6, 0, 4, 0, 7, 0, 8 },
+            //{ 0, 1, 0, 0, 4, 0, 0, 0 },
+            //{ 8, 7, 5, 0, 0, 2, 0, 0 },
+            //{ 0, 0, 1, 0, 0, 0, 3, 5 },
+            //{ 0, 2, 3, 0, 0, 5, 0, 7 },
+            //{ 2, 0, 8, 0, 0, 0, 7, 3 },
+            //{ 0, 0, 0, 0, 0, 0, 2, 0 }
+            //};
+
+            //int[,] zagonetka = new int[,] {
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 },
+            //{ 0, 0, 0, 0, 0, 0, 0, 0 }
+            //};
+
+            int[,] zagonetka = new int[,] {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 4 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 5 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 7 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 8 },
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            };
+
+            Sudoku sudoku = new Sudoku();
+            sudoku.Zagonetka = zagonetka;
+
+            sudoku.IspisiSudoku();
+            Console.Write("______________________ \n\n");
+
+            if (sudoku.RijesiSudoku())
+            {
+                sudoku.IspisiSudoku();
+            }
+            else
+            {
+                Console.Write("Nema rješenja");
+            }
+
+
+            watch.Stop();
+            Console.WriteLine("\n" + watch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver
+{
+    class Sudoku
+    {
+		public int[,] Zagonetka { set; get; }
+
+		public List<int> PronadiKandidate(int redak, int stupac)
+		{
+			List<int> kandidati = new List<int>();
+			int velicina = Zagonetka.GetLength(0);
+
+			for (int broj = 1; broj <= velicina; broj++)
+			{
+				bool nijeKandidat = false;
+
+				for (int i = 0; i < velicina; i++)
+				{
+					if (Zagonetka[redak, i] == broj ||
+						Zagonetka[i, stupac] == broj)
+					{
+						nijeKandidat = true;
+						break;
+					}
+				}
+				if (!nijeKandidat)
+                    kandidati.Add(broj);
+			}
+			return kandidati;
+		}
+
+		public bool RijesiSudoku()
+		{
+			int redak = -1;
+			int stupac = -1;
+			int velicina = Zagonetka.GetLength(0);
+			List<int> kandidati = new List<int>();
+
+			for (int i = 0; i < velicina; i++)
+			{
+				bool jediniKandidat = false;
+				for (int j = 0; j < velicina; j++)
+				{
+					if (Zagonetka[i, j] == 0)
+					{
+						List<int> noviKandidati = PronadiKandidate(i, j);
+						if (redak < 0 ||
+							noviKandidati.Count < kandidati.Count)
+						{
+							redak = i;
+							stupac = j;
+							kandidati = noviKandidati;
+						}
+						if (kandidati.Count == 1)
+						{
+							jediniKandidat = true;
+							break;
+						}
+					}
+				}
+				if (jediniKandidat)
+					break;
+			}
+
+			if (redak < 0)
+			{
+				return true;
+			}
+			
+			else
+				for (int i = 0; i < kandidati.Count; i++)
+                {
+					Zagonetka[redak, stupac] = kandidati[i];
+
+					if (RijesiSudoku())
+					{
+						return true;
+					}
+					else Zagonetka[redak, stupac] = 0;
+                }
+			return false;
+		}
+
+		public void IspisiSudoku()
+		{
+			int velicina = Zagonetka.GetLength(0);
+			for (int redak = 0; redak < velicina; redak++)
+			{
+				for (int stupac = 0; stupac < velicina; stupac++)
+				{
+					Console.Write(Zagonetka[redak, stupac]);
+					Console.Write(" ");
+				}
+				Console.Write("\n");
+			}
+		}
+
+	}
+}
